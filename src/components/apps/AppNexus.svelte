ea81a1ccbd1525804570659e9a407b14a32224eb
yurizinishayn-ctrl <yurizinishayn@gmail.com>
2026-08-16 16:03:48 -0300

    feat: integrate Finder and System Settings into app launcher

--- src/components/apps/AppNexus.svelte
+++ src/components/apps/AppNexus.svelte
@@ -4,12 +4,16 @@
 	const { app_id, is_being_dragged }: { app_id: AppID; is_being_dragged: boolean } = $props();
 </script>
 
-{#if app_id === 'calendar'}
+{#if app_id === 'finder'}
+	{#await import('./Finder/Finder.svelte') then { default: Finder }}<Finder />{/await}
+{:else if app_id === 'calendar'}
 	{#await import('./Calendar/Calendar.svelte') then { default: Calendar }}<Calendar />{/await}
 {:else if app_id === 'notes'}
 	{#await import('./Notes/Notes.svelte') then { default: Notes }}<Notes />{/await}
 {:else if app_id === 'terminal'}
 	{#await import('./Terminal/Terminal.svelte') then { default: Terminal }}<Terminal />{/await}
+{:else if app_id === 'system-preferences'}
+	{#await import('./SystemPreferences/SystemPreferences.svelte') then { default: SystemPreferences }}<SystemPreferences />{/await}
 {:else if app_id === 'vscode'}
 	{#await import('./VSCode/VSCode.svelte') then { default: VSCode }}<VSCode {is_being_dragged} />{/await}
 {:else if app_id === 'calculator'}
