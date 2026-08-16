2589dd8fffc15372098969dd85d13decaa6abab2
yurizinishayn-ctrl <yurizinishayn@gmail.com>
2026-08-16 18:06:48 -0300

    ui: restore Finder dock icon from project asset

---
+++ src/components/SystemUI/FinderDockIconFix.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<style>
+		.dock-open-app-button.finder .icon-shell {
+			background-image: url('${import.meta.env.BASE_URL}app-icons/finder/256.png');
+			background-size: 100% 100%;
+			background-position: center;
+			background-repeat: no-repeat;
+			box-shadow: none;
+		}
+
+		.dock-open-app-button.finder .icon-shell > svg {
+			opacity: 0;
+			height: 100%;
+			width: 100%;
+		}
+	</style>
+</svelte:head>
