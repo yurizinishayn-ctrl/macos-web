0419797228677e1a61bb58ff6f261c83168e8099
yurizinishayn-ctrl <yurizinishayn@gmail.com>
2026-08-19 20:05:01 -0300

    restore: recover Desktop entry point

---
+++ src/components/Desktop/Desktop.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Dock from '../Dock/Dock.svelte';
+	import TopBar from '../TopBar/TopBar.svelte';
+	import Wallpaper from '../apps/WallpaperApp/Wallpaper.svelte';
+	import BootupScreen from './BootupScreen.svelte';
+	import ContextMenu from './ContextMenu.svelte';
+	import SystemUpdate from './SystemUpdate.svelte';
+	import WindowsArea from './Window/WindowsArea.svelte';
+	import SystemFeatures from '../SystemUI/SystemFeatures.svelte';
+	import FinderDockIconFix from '../SystemUI/FinderDockIconFix.svelte';
+
+	const isMac = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+
+	if (!isMac) {
+		Promise.all([
+			import('@fontsource/inter/latin-ext-300.css'),
+			import('@fontsource/inter/latin-ext-400.css'),
+			import('@fontsource/inter/latin-ext-500.css'),
+			import('@fontsource/inter/latin-ext-600.css'),
+		]);
+	}
+	/** @type {HTMLElement} */
+	let mainEl;
+</script>
+
+<div bind:this={mainEl} class="container">
+	<Wallpaper />
+	<main>
+		<TopBar />
+		<WindowsArea />
+		<Dock />
+	</main>
+	<SystemFeatures />
+	<FinderDockIconFix />
+	<BootupScreen />
+	<SystemUpdate />
+	<ContextMenu target_element={mainEl} />
+</div>
+
+<style>
+	.container { height:100%; width:100%; position:relative; isolation:isolate; background:#202124; overflow:hidden; }
+	main { height:100%; width:100%; position:relative; z-index:1; display:grid; grid-template-rows:auto 1fr auto; }
+</style>
